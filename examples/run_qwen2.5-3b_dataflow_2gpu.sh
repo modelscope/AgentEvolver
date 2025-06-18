783b2e426220452eed92629c1ef8d2d856092723
Zhai Yunpeng <zhaiyunpeng.zyp@alibaba-inc.com>
2025-06-18 13:50:37 +0800

    except env error

--- examples/run_qwen2.5-3b_dataflow_2gpu.sh
+++ examples/run_qwen2.5-3b_dataflow_2gpu.sh
@@ -10,14 +10,14 @@ CONFIG_PATH="$PROJECT_DIR/beyondagent/config"
 # completion_callback=none
 env_url=http://localhost:8000
 current_time=$(date "+%Y%m%d_%H%M%S")
-log_file="python_log_${current_time}.log"
+log_file="dlc_log_${current_time}.log"
 
 python3 -m beyondagent.main_ppo \
     --config-path="$CONFIG_PATH" \
     --config-name='beyond_agent_dataflow' \
     beyond_agent.env_url=$env_url \
     algorithm.adv_estimator=grpo \
-    data.train_batch_size=2 \
+    data.train_batch_size=16 \
     data.max_prompt_length=4096 \
     data.max_response_length=20480 \
     data.filter_overlong_prompts=True \
@@ -30,7 +30,7 @@ python3 -m beyondagent.main_ppo \
     actor_rollout_ref.model.path=/mnt/data_cpfs/yunpeng.zyp/models/Qwen2.5-3B-Instruct \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
-    actor_rollout_ref.actor.ppo_mini_batch_size=2 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=16 \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=1 \
     actor_rollout_ref.actor.use_kl_loss=True \
     actor_rollout_ref.actor.kl_loss_coef=0.001 \
@@ -44,11 +44,11 @@ python3 -m beyondagent.main_ppo \
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.rollout.mode=async \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
-    actor_rollout_ref.rollout.n=2 \
+    actor_rollout_ref.rollout.n=8 \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=1 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
     algorithm.use_kl_in_reward=False \
-    trainer.n_gpus_per_node=2 \
+    trainer.n_gpus_per_node=8 \
     trainer.critic_warmup=0 \
     trainer.logger=['console'] \
     trainer.project_name='gsm8k_async_rl' \
