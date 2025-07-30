8f596a097dcd4e3265a85f2887ca3642b3a42b13
cc <cc445845@alibaba-inc.com>
2025-07-30 16:54:05 +0800

    fix: remove wrong imports

--- beyondagent/module/task_manager/strategies/deduplication/prompts/__init__.py
+++
@@ -1,9 +0,0 @@
-from .prompt_explore import get_agent_interaction_system_prompt
-from .prompt_summarize import get_task_summarize_prompt, parse_tasks_from_response
-
-
-__all__=[
-    "get_agent_interaction_system_prompt",
-    "get_task_summarize_prompt",
-    "parse_tasks_from_response",
-]
