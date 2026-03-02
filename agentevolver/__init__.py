f84edcae93139c0c6b86ac641fb418fc37fb1899
haosenwang1018 <167664334+haosenwang1018@users.noreply.github.com>
2026-03-02 12:04:49 +0000

    feat: add get_version() helper

--- agentevolver/__init__.py
+++ agentevolver/__init__.py
@@ -2,4 +2,9 @@
 __version__ = "0.1.0"  # ⭐ Sets the version of the project
 
 
+def get_version():
+    """Return the AgentEvolver version."""
+    return __version__
+
+
 from agentevolver.utils.vsdb import vscode_conditional_breakpoint as bp
